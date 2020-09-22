76cdb7d79feec71ab0a87aa00e6c58ec8ed51a2f
cleytonchagasbr <cleyton-chagas@hotmail.com>
2020-09-22 13:39:54 -0300

    update eslint and prettier ok

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 
-export default function Home({ title = "Next js Cleyton" }) {
+export default function Home({ title = `Next js Cleyton` }) {
   return (
     <div className="container">
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
     </div>
   )
 }
