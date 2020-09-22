c41a2e62b301d75476c00f74201101a8884ee525
cleytonchagasbr <cleyton-chagas@hotmail.com>
2020-09-22 15:31:34 -0300

    Config jest

---
+++ client/jest.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_module', '/.next'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
